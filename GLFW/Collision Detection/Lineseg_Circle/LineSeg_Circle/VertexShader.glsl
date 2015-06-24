d3be3d7596a57e0645b0afa2c1563794e4733d56
Srinivasan T <sxt5244@g.rit.edu>
2015-06-24 19:23:39 -0400

    Line Segment Cricle 2D

---
+++ GLFW/Collision Detection/Lineseg_Circle/LineSeg_Circle/VertexShader.glsl
@@ -0,0 +1,47 @@
+/*
+Title: Line Segment and Circle 2D intersection
+File Name: VertexShader.glsl
+Copyright © 2015
+Author: Srinivasan Thiagarajan
+Original authors: Brockton Roth
+Written under the supervision of David I. Schwartz, Ph.D., and
+supported by a professional development seed grant from the B. Thomas
+Golisano College of Computing & Information Sciences
+(https://www.rit.edu/gccis) at the Rochester Institute of Technology.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Description:
+This is an example to detect the intersection of a line segment and circle in 2D. 
+You can control the two end-points of the line segment, and move them using "w,a,s,d" and "i,j,k,l" respectively.
+The line turns blue when an intersection is detected, and turns red when there is no intersection.
+The program first checks if either of the end-points lie within the circle (which is stationary), if so, collision is detected.
+If neither of the end-points are inside the circle, then it check for the closest point to the circle's center, which also lies on the line,
+and checks if that point lies inside the circle. If so, collision is detected else not.
+*/
+
+#version 400 core // Identifies the version of the shader, this line must be on a separate line from the rest of the shader code
+ 
+layout(location = 0) in vec3 in_position;	// Get in a vec3 for position
+layout(location = 1) in vec4 in_color;		// Get in a vec4 for color
+
+out vec4 color; // Our vec4 color variable containing r, g, b, a
+
+uniform vec2 translation_2D; // Our uniform MVP matrix to modify our position values
+
+void main(void)
+{
+	color = in_color;	// Pass the color through
+	gl_Position = vec4(in_position.x + translation_2D.x, in_position.y + translation_2D.y, in_position.z, 1.0); //w is 1.0, also notice cast to a vec4
+}
