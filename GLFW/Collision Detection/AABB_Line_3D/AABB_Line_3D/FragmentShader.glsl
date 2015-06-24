83e3484561e50c55241a07d2fab873152c1e7066
Srinivasan T <sxt5244@g.rit.edu>
2015-06-24 19:08:50 -0400

    AABB Line 3D

---
+++ GLFW/Collision Detection/AABB_Line_3D/AABB_Line_3D/FragmentShader.glsl
@@ -0,0 +1,51 @@
+/*
+Title: AABB-Line 3D collision Detection
+File Name: FragmentShader.glsl
+Copyright © 2015
+Original authors: Srinivasan Thiagarajan
+Written under the supervision of David I. Schwartz, Ph.D., and
+supported by a professional development seed grant from the B. Thomas
+Golisano College of Computing & Information Sciences
+(https://www.rit.edu/gccis) at the Rochester Institute of Technology.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Description:
+This is an example to detect the intersection of a line and AABB in 3D.
+We extend the concept of finding the point of intersection between a plane and a line
+to detect where the line enter and exits the box. The box has 6 planes : 3 parallel to the other 3. 
+We take these sets of parallel planes and find where the line intersects them both. 
+The nearest one acts as entry point and the farthest acts as exit.
+We combine the results of all 3 sides, and find the FARTHEST POINT OF ENTRY AND CLOSEST POINT OF EXIT.
+If the entry point is farther than the exit, then the line segment exits that set of planes before 
+entering the other set of planes. Thus no collision is detected. 
+
+Use Mouse to move in x-y plane, and "w and s" to move in z axis.
+
+References:
+(Pg: 179) RealTime collision detection by Christer Ericson
+AABB-2D by Brockton Roth
+*/
+
+
+#version 400 core // Identifies the version of the shader, this line must be on a separate line from the rest of the shader code
+
+layout(location = 0) out vec4 out_color; // Establishes the variable we will pass out of this shader.
+
+in vec4 color;	// Take in a vec4 for color
+ 
+void main(void)
+{
+	out_color = color; // Set our out_color equal to our in color, basically making this a pass-through shader.
+}
