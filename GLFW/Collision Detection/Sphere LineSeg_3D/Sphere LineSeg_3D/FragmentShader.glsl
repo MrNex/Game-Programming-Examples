98bad07e77f0e8f5f6b02e07611a7685a5e6ff6c
Srinivasan T <sxt5244@g.rit.edu>
2015-06-24 19:36:40 -0400

    Sphere Line Segment 3D

---
+++ GLFW/Collision Detection/Sphere LineSeg_3D/Sphere LineSeg_3D/FragmentShader.glsl
@@ -0,0 +1,45 @@
+/*
+Title: Sphere-Line Segment 3D collision Detection
+File Name: Fragmentshader.glsl
+Copyright © 2015
+Original authors: Srinivasan Thiagarajan
+Written under the supervision of David I. Schwartz, Ph.D., and
+supported by a professional development seed grant from the B. Thomas
+Golisano College of Computing & Information Sciences
+(https://www.rit.edu/gccis) at the Rochester Institute of Technology.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Description:
+This is an example to detect the intersection of a line segment and circle in 3D.
+The program first checks if either of the end-points lie within the circle (which is stationary), if so, collision is detected.
+If neither of the end-points are inside the circle, then it check for the closest point to the circle's center, which also lies on the line,
+and checks if that point lies inside the circle. If so, collision is detected else not.
+Use Mouse to move in x-y plane, and "w and s" to move in z axis.
+
+References:
+AABB-2D by Brockton Roth
+*/
+
+
+#version 400 core // Identifies the version of the shader, this line must be on a separate line from the rest of the shader code
+
+layout(location = 0) out vec4 out_color; // Establishes the variable we will pass out of this shader.
+
+in vec4 color;	// Take in a vec4 for color
+ 
+void main(void)
+{
+	out_color = color; // Set our out_color equal to our in color, basically making this a pass-through shader.
+}
